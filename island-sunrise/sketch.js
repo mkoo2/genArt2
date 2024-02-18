7b4ff78a9c80bbadab4077f63501c32137807e56
Mark K <mjhtracy@gmail.com>
2024-02-17 21:05:09 -0800

    update to sunrise

--- island-sunrise/sketch.js
+++ island-sunrise/sketch.js
@@ -13,7 +13,7 @@ function setup() {
 	sky = createGraphics(width, height);
 	water = createGraphics(width, height);
 
-	sunriseColor = "orange";
+	sunriseColor = "red";
 	buildEnviron();
 
 	imageMode(CENTER);
@@ -55,8 +55,13 @@ function buildEnviron () {
 	}
 	//sun rays on water
 	for (let a = 0; a < PI; a += PI / 120) {
-		water.stroke(200, 0, 0, 80 * sin(a)); // red
-		water.strokeWeight(height / 80);
+		if (sunriseColor == 'orangle') {
+			water.stroke(255, 165, 0, 80 * sin(a)); // 
+
+		} else {
+			water.stroke(200, 0, 0, 80 * sin(a)); // red
+		}
+	water.strokeWeight(height / 80);
 		water.line(width / 2 + cos(a) * height / 20, 2 * height / 3 + height / 100, width / 2 + cos(a) * width, 2 * height / 3 + sin(a) * height);
 
 	}
@@ -78,9 +83,9 @@ function mousePressed() {
 
 function draw() {
 	translate(width / 2, height / 2);
-	if (frameCount > 1) {
-		if (frameCount == 1500) {
-			sunriseColor = 'red';
+	if (frameCount > 1 && frameCount < 10000) {
+		if (frameCount == 2500) {
+			sunriseColor = 'orange';
 			buildEnviron();
 		}
 		scale(1.05 + sin(frameCount / 400) / 20)
@@ -89,7 +94,7 @@ function draw() {
 		fill('gold');
 		noStroke();
 		// 100 - 15 + ; height = 600
-		ellipse(0, height / 6 - height / 40 + frameCount / 200, height / 20);
+		ellipse(0, height / 6 + height / 40 - frameCount / 200, height / 20);
 		image(water, 0, 0);
 		if (island) drawIsland();
 		let alpha = max(255 - frameCount / 3, 0);
@@ -100,7 +105,12 @@ function draw() {
 			y += sin(a) * cos((8 * a) + frameCount / 200) * height / 36;
 			let w = (1 - sin(a)) * (width / 4) + noise((3 * a) + frameCount / 400) * width / 8;
 			strokeWeight(map(a, HALF_PI, PI, 3, 0.5  ))
-			stroke(255, 255, 255, map(a, HALF_PI, PI, 0, 50));
+			if (sunriseColor == 'orangle') {
+				stroke(255, 165, 0, map(a, HALF_PI, PI, 0, 50)); // orange
+
+			} else {
+				stroke(255, 255, 255, map(a, HALF_PI, PI, 0, 50)); // red
+			}
 			line(-width / 2, y, width / 2, y);
 			stroke(200, 0, 0, map(a, HALF_PI, PI, 0, 150) * dim);
 			line(-w, y, w, y);
