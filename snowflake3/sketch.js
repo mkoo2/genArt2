443764ab559824851345b31ea8d96fac95909fcf
mkoo2 <mjhtracy@gmail.com>
2026-02-21 23:06:34 -0800

    update

--- snowflake3/sketch.js
+++ snowflake3/sketch.js
@@ -2,9 +2,11 @@
 // <script src="https://cdn.jsdelivr.net/npm/p5@1.11.11/lib/p5.js"></script>
 // <script src="https://cdn.jsdelivr.net/npm/p5@1.11.11/lib/addons/p5.sound.min.js"></script>
 // <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.2/addons/p5.dom.min.js"></script>
+let snow = [];
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  snow.push(new Snowflake(300,200));
 }
 
 function mousePressed() {
@@ -14,5 +16,8 @@ function mousePressed() {
 function draw() {
   background(0);
 
+  for (flake of snow){
+    flake.render();
+  }
 
 }
---
+++ snowflake3/snowflake.js
@@ -0,0 +1,16 @@
+
+class Snowflake{
+  
+  constructor() {
+    this.pos = createVector();
+    this.vel = createVector();
+    this.acc = createVector();
+  }
+  
+  render(){
+    stroke(255);
+    strokeWeight(4);
+    point(this.pos.x, this.pos.y);
+  }
+}
+  
