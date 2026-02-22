25cc876cd1bbaed978f73354c709223e48f91a03
mkoo2 <mjhtracy@gmail.com>
2026-02-21 22:56:39 -0800

    update snowflake

---
+++ snowflake3/sketch.js
@@ -0,0 +1,18 @@
+
+// <script src="https://cdn.jsdelivr.net/npm/p5@1.11.11/lib/p5.js"></script>
+// <script src="https://cdn.jsdelivr.net/npm/p5@1.11.11/lib/addons/p5.sound.min.js"></script>
+// <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.2/addons/p5.dom.min.js"></script>
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function mousePressed() {
+
+}
+
+function draw() {
+  background(0);
+
+
+}
